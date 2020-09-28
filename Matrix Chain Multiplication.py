35c0f0c5c3e9a1f068db0d3904ac515ae791d240
Geeky-Star <nimratkaur540@gmail.com>
2020-09-28 22:16:32 +0530

    Update Matrix Chain Multiplication.py

--- Matrix Chain Multiplication.py
+++ Matrix Chain Multiplication.py
@@ -19,3 +19,27 @@ def MatrixChainOrder(p,i,j):
 
     return _min
 
+
+#Using Dynamic Programming
+
+
+import sys
+
+def MatrixChainOrder(p,n):
+
+    dp = [[0 for i in range(n)] for j in range(n)]
+
+    for i in range(1,n):
+        dp[i][i]=0
+
+    for L in range(2,n):
+        for i in range(1,n-L+1):
+            j = i+L-1
+            dp[i][j] = sys.maxsize
+            for k in range(i,j):
+                q = dp[i][k] + dp[k+1][j] + p[i-1]*p[k]*p[j]
+
+                if q<dp[i][j]:
+                    dp[i][j]=q
+
+    return dp[1][n-1]
