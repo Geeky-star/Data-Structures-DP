4a04ebe9506bf72b36a055b0f349112c689823ef
Geeky-Star <nimratkaur540@gmail.com>
2021-08-01 15:07:24 +0530

    Create LongestCommon Substring.py

---
+++ LongestCommon Substring.py
@@ -0,0 +1,16 @@
+ def longestCommonSubstr(self, S1, S2, n, m):
+        
+        dp =[[0 for i in range(m+1)] for j in range(n+1)]
+        result = 0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                
+                if S1[i-1]==S2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                    result = max(result,dp[i][j])
+                    
+                else:
+                    dp[i][j] = 0
+                    
+        
+        return result
